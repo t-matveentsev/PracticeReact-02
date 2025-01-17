f2e98589c266c6cd367b65c1077d6048636e0b63
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-01-17 22:09:02 +0100

    color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Gallery from "./Gallery/Gallery";
 import Counter from "./Counter/Counter";
+import ColorPicker from "./ColorPicker/ColorPicker";
 
 export default function App() {
   return (
     <div>
       <Gallery />
       <Counter />
+      <ColorPicker />
     </div>
   );
 }
